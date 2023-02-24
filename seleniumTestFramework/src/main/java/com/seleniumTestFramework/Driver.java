9906de75f2592e0ba47bae3e9550b18f9cd9c12c
wilderchris <94183821+wilderchris@users.noreply.github.com>
2023-02-24 09:01:03 -0600

    updating args data

--- seleniumTestFramework/src/main/java/com/seleniumTestFramework/Driver.java
+++ seleniumTestFramework/src/main/java/com/seleniumTestFramework/Driver.java
@@ -7,8 +7,14 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO would like to make this a command line app with args 
 		//to decide type of test, file to use, and reporting type
+		
+		for (int i = 0; i < args.length;i++ ) {
+			System.out.println("arg["+i+" ] = " + args[i]);
+		}
+		
 		KeywordEngine ke = new KeywordEngine();
-		ke.startExecution("sheet1");;
+		ke.startExecution("sheet1");
+		
 	}
 
 }
