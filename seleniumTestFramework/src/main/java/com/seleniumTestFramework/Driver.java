2b4978885ccc46846dbf6a315b89d34af6cc0841
wilderchris <94183821+wilderchris@users.noreply.github.com>
2023-02-24 12:37:10 -0600

    Update Driver.java

--- seleniumTestFramework/src/main/java/com/seleniumTestFramework/Driver.java
+++ seleniumTestFramework/src/main/java/com/seleniumTestFramework/Driver.java
@@ -7,7 +7,11 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO would like to make this a command line app with args 
 		//to decide type of test, file to use, and reporting type
-		
+		if ( args.length == 0) {
+			System.out.println("Usage for app:");
+			System.out.println("Driver [type] [file] [?]");
+		}
+			
 		for (int i = 0; i < args.length;i++ ) {
 			System.out.println("arg["+i+" ] = " + args[i]);
 		}
