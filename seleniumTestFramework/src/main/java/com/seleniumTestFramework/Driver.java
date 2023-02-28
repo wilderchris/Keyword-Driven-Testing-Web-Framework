c2621855d7824bda7f9f9b21af9f931d52d9b9fb
wilderchris <94183821+wilderchris@users.noreply.github.com>
2023-02-28 11:12:16 -0600

    Update Driver.java

--- seleniumTestFramework/src/main/java/com/seleniumTestFramework/Driver.java
+++ seleniumTestFramework/src/main/java/com/seleniumTestFramework/Driver.java
@@ -5,20 +5,24 @@ import com.seleniumTestFramework.keywordEngine.KeywordEngine;
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO would like to make this a command line app with args 
-		//to decide type of test, file to use, and reporting type
-		if ( args.length == 0) {
+		// TODO would like to make this a command line app with args
+		// to decide type of test, file to use, and reporting type
+		if (args.length == 0) {
 			System.out.println("Usage for app:");
 			System.out.println("Driver [type] [file] [?]");
 		}
+
+		for (int i = 0; i < args.length; i++) {
+			System.out.println("arg[" + i + " ] = " + args[i]);
+
+			KeywordEngine ke = new KeywordEngine();
+			ke.startExecution("sheet1");
 			
-		for (int i = 0; i < args.length;i++ ) {
-			System.out.println("arg["+i+" ] = " + args[i]);
+			if (args[0] == "test") {
+
+			}
 		}
-		
-		KeywordEngine ke = new KeywordEngine();
-		ke.startExecution("sheet1");
-		
+
 	}
 
 }
